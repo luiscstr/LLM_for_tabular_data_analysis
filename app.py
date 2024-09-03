fee90ed3f188678cabd6a92f84c11800b1b7b9e1
Luis Castro <66234017+luiscstr@users.noreply.github.com>
2024-09-02 21:02:09 -0400

    Upload file function

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 import gradio as gr
-from sqlchain import agent_executor
-
-
-
-def ask_bot(text):
-    output=agent_executor.invoke(text)
-    return output
+from  chatbot import ChatBot
+from dotenv import load_dotenv
+from upload_file import load_and_save_file
+load_dotenv()
 
 
 def main():
@@ -15,7 +12,7 @@ def main():
         with gr.Row() as row_one:
             chatbot=gr.Chatbot(
                 [],
-                element_id='chatbot',
+                elem_id='chatbot',
                 bubble_full_width=False,
                 height=500,
                 avatar_images=("images/user.png","images/chatbot.jpg")
@@ -23,7 +20,7 @@ def main():
              # **Adding like/dislike icons
                 #chatbot.like(UISettings.feedback, None, None)
         #Second row:text input
-        with gr.Row as row_two:():
+        with gr.Row() as row_two:
             input_text=gr.Textbox(
                 lines=4,
                 scale=8,
@@ -31,27 +28,30 @@ def main():
                 container=False,
             )
         #Third row:button
-        with gr.Row as row_three:():
+        with gr.Row() as row_three:
             submit_btn=gr.Button(value='Submit text')
+            file_input=gr.UploadButton(
+                 label="📁 Upload CSV or XLSX files",
+                file_types=['.csv','.xls','.xlsx'])
             clear_btn=gr.ClearButton([input_text,chatbot])
+        output_message = gr.Textbox(label="Message", interactive=False)
+        output_table = gr.DataFrame(label="Preview of the Data")
+   
         #Process (can be submitted with enter or with click button)
-        output=input_text.submit(fn=Chatbot.respond,
+        file_input.upload(fn=load_and_save_file,inputs=[file_input],outputs=[output_message,output_table])
+    
+        output=input_text.submit(fn=ChatBot.respond,
+                                 inputs=[chatbot,input_text],
+                                 outputs=[input_text,chatbot],
+                                 queue=False).then(lambda: gr.Textbox(interactive=True),
+                                                   None,[input_text],queue=False)
+        output=submit_btn.click(fn=ChatBot.respond,
                                  inputs=[chatbot,input_text],
                                  outputs=[input_text,
                                           chatbot],
-                                 queue=False).then(lambda gr: gr.Textbox(interactive=True),
-                                                   None,[input_text],queue=False)
-        output=submit_btn.click(fn=Chatbot.respond,
-                                 inputs=[chatbot,input_text],
-                                 outputs=[input_text,
-                                          chatbot],
-                                 queue=False).then(lambda gr: gr.Textbox(interactive=True),
+                                 queue=False).then(lambda: gr.Textbox(interactive=True),
                                                    None,[input_text],queue=False)
         
-        
-        
-        
-      
     
     querydb.launch()
 
